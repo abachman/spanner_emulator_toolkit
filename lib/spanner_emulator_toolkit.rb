204bb3efe75756129c9d4fb7e08d7c3024581497
Adam Bachman <abachman@recurly.com>
2023-12-05 11:57:44 -0500

    bundle gem spanner_emulator_toolkit

---
+++ lib/spanner_emulator_toolkit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "spanner_emulator_toolkit/version"
+
+module SpannerEmulatorToolkit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/spanner_emulator_toolkit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SpannerEmulatorToolkit
+  VERSION = "0.1.0"
+end
